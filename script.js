8b372e7b6c32ce6d253613f246380d581befeecc
Hannes <142382297+waechtler-hannes@users.noreply.github.com>
2024-10-24 12:06:14 +0200

    bring the game from console to ui
    Deleted the "playGame" function, where you can play several rounds
    against the computer.
    Deleted the function that fixes player input if the player types "rock"
    instead of "Rock".
    Deleted the function for getting players Input from alert.
    Added 3 buttons for "Rock", "Paper" and "Scissors" with Event-
    Listeners, so that everytime you push a button, a new Round starts.

--- script.js
+++ script.js
@@ -2,70 +2,46 @@ function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     switch (randomNumber) {
         case 1:
-            return "rock";
+            return "Rock";
             break;
         case 2:
-            return "paper";
+            return "Paper";
             break;
         case 3:
-            return "scissor";
+            return "Scissor";
             break;
-    }
-}
-
-function getHumanChoice() {
-    let humanChoice = prompt('Pick your choice and type "rock", "paper" or "scissor":').toLowerCase();
-    if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissor") {
-        return humanChoice;
-    } else {
-        alert('Unvalid input, try again.')
     }
 }
 
 function playRound(computerChoice, humanChoice) {
-    if ((computerChoice === "rock" && humanChoice === "paper") || (computerChoice === "paper" && humanChoice === "scissor") || (computerChoice === "scissor" && humanChoice === "rock")) {
-        console.log(`\nYou win! ${firstToUpperCase(humanChoice)} beats ${firstToUpperCase(computerChoice)}.`);
-        return "win";
-    } else if ((computerChoice === "rock" && humanChoice === "scissor") || (computerChoice === "paper" && humanChoice === "rock") || (computerChoice === "scissor" && humanChoice === "paper")) {
-        console.log(`\nYou lose! ${firstToUpperCase(computerChoice)} beats ${firstToUpperCase(humanChoice)}.`);
-        return "lose";
-    } else if ((computerChoice === "rock" && humanChoice === "rock") || (computerChoice === "paper" && humanChoice === "paper") || (computerChoice === "scissor" && humanChoice === "scissor")) {
+    if ((computerChoice === "Rock" && humanChoice === "Paper") || 
+    (computerChoice === "Paper" && humanChoice === "Scissor") || 
+    (computerChoice === "Scissor" && humanChoice === "Rock")) {
+        console.log(`\nYou win! ${humanChoice} beats ${computerChoice}.`);
+    } else if ((computerChoice === "Rock" && humanChoice === "Scissor") || 
+    (computerChoice === "Paper" && humanChoice === "Rock") || 
+    (computerChoice === "Scissor" && humanChoice === "Paper")) {
+        console.log(`\nYou lose! ${computerChoice} beats ${humanChoice}.`);
+    } else if ((computerChoice === "Rock" && humanChoice === "Rock") || 
+    (computerChoice === "Paper" && humanChoice === "Paper") || 
+    (computerChoice === "Scissor" && humanChoice === "Scissor")) {
         console.log(`\nIt's a Draw!`);
     } else {
         console.log(`\nUnvalid Input!`);
     }
 }
 
-function firstToUpperCase(string) {
-    let firstLetter = string.at(0).toUpperCase();
-    let lastLetters = string.substr(1,string.length);
-    return firstLetter.concat(lastLetters);
-}
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    console.log("First to win 3 times wins the game!")
-    while(humanScore < 3 && computerScore < 3) {
-        let result = playRound(getComputerChoice(), getHumanChoice());
-        switch (result) {
-            case "win":
-                humanScore++;
-                break;
-            case "lose":
-                computerScore++;
-                break;
-            default:
-                break;
-        }
-        console.log(`Your Score:           ${humanScore}`);
-        console.log(`Computer's Score:     ${computerScore}`);
+const buttons = document.querySelectorAll("button");
+document.addEventListener("click", (e) => {
+    switch (e.target.textContent) {
+        case "Rock":
+            playRound(getComputerChoice(), "Rock");
+            break;
+        case "Paper":
+            playRound(getComputerChoice(), "Paper");
+            break;
+        case "Scissors":
+            playRound(getComputerChoice(), "Scissor");
+            break;
     }
-    if (humanScore > computerScore) {
-        console.log('\nCongratulations, you won the game!');
-    } else {
-        console.log('\nToo bad, you lost the game!')
-    }
-}
-
-playGame();
+});
